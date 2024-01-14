8cd764e879a67976cbe51c22a061f897e37e4026
khoa.nguyen <khoanguyen0109.99@gmail.com>
2024-01-14 17:06:04 +0700

    discount

--- src/controller/v1/discounts/index.ts
+++ src/controller/v1/discounts/index.ts
@@ -6,7 +6,7 @@ export async function validateVoucher(req, res, next) {
   const { voucher } = req.body;
   const sheet = (await getDoc('discount')) as GoogleSpreadsheetWorksheet;
   const data = (await sheet.getRows()).find((item) => item.get('voucher') === voucher).toObject();
-  const now = new DateTime.now();
+  const now = DateTime.now();
 
   if (!data || (data.expired && new DateTime(data.expired) < now)) {
     return res.status(404).json({ message: 'Not Found' });
